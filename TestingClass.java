d1d3fc6ddf7a5c08536aa4d08368fb11fb0abffc
Grzyboo <grzyboo111@gmail.com>
2016-03-23 13:51:20 +0100

    Created a simple class

---
+++ TestingClass.java
@@ -0,0 +1,16 @@
+package MatrixApplet.Dialogs;
+
+
+public class TestingClass {
+
+    int x, y;
+
+    public TestingClass(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void print() {
+        System.out.println("x: " + x + " | y: " + y);
+    }
+}
