0bf1b3c9bdd8af71ba8132d34889e6e76a638f57
Grzyboo <grzyboo111@gmail.com>
2016-03-23 13:56:19 +0100

    Next changes; Added name to TestingClass

--- TestingClass.java
+++ TestingClass.java
@@ -3,15 +3,26 @@ package MatrixApplet.Dialogs;
 
 public class TestingClass {
 
+    Sting name;
     int x, y;
 
     public TestingClass(int x, int y) {
         this.x = x;
         this.y = y;
+
+        name = "Unnamed";
     }
 
     public void print() {
         System.out.println("The position is:");
         System.out.println("x: " + x + " | y: " + y);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
